96f33c9f3395b53344a6acfb858736f907e2d9cc
yamAhm21 <yaminahmed2201@gmail.com>
2026-05-03 00:49:32 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Beyond Elemental Forever!!",
         // Activity type number (0 = Playing).
         type: 0, 
       },
